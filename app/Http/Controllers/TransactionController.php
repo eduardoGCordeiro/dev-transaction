bca7d00c07a7eacb531a4a9211e510d54f57c1ef
eduardoGCordeiro <eduardoguilhermecordeiro@gmail.com>
2021-05-30 16:04:18 -0300

    Adicionando a função/rota para realização de transações.

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -6,6 +6,11 @@ use App\Http\Controllers\Controller;
 use App\Rules\CheckBalancePayer;
 use App\Rules\PayerUserType;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Exceptions\TransactionException;
+use App\Exceptions\MockTransactionException;
+use App\Models\Wallet;
+use App\Models\Transaction;
 
 class TransactionController extends Controller
 {
@@ -26,6 +31,41 @@ class TransactionController extends Controller
             ]
         ]);
 
-        return response()->json(['message' => 'Transaction complete with success!'], 200);
+        DB::beginTransaction();
+
+        try {
+            $transaction = new Transaction();
+            $transaction->value = $request->input('value');
+            $transaction->payer_wallet_id = $request->input('payer_wallet_id');
+            $transaction->payee_wallet_id = $request->input('payee_wallet_id');
+            $transaction->save();
+
+            $wallet_payeer = Wallet::find($request->input('payer_wallet_id'));
+
+            if ($transaction->value > $wallet_payeer->balance) {
+                throw new TransactionException('Insufficient balance to complete the transaction!');
+            }
+
+            if ($transaction->value <= $wallet_payeer->balance) {
+                $wallet_payeer->balance -= $transaction->value;
+                $wallet_payeer->save();
+            }
+
+            $wallet_payee = Wallet::find($request->input('payee_wallet_id'));
+            $wallet_payee->balance += $transaction->value;
+            $wallet_payee->save();
+
+            DB::commit();
+            return response()->json(['message' => 'The transaction was completed successfully!'], 200);
+        } catch (TransactionException $e) {
+            DB::rollback();
+            return response()->json(['message' => $e->getMessage()], 422);
+        } catch (MockTransactionException $e) {
+            DB::rollback();
+            return response()->json(['message' => $e->getMessage()], 503);
+        } catch (\Exception $e) {
+            DB::rollback();
+            return response()->json(['message' => 'The transaction failed, please try again later!'], 500);
+        }
     }
 }
